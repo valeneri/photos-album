12674cd9319d686da28d1998a34a67dc396fda3d
Valentin <valentin.nerin@gmail.com>
2020-05-17 08:29:42 +0200

    Add date formatting utils

---
+++ frontend/src/utils/utils.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// date from backend come as DD-MM-YYYY format, then output it as  new Date(YYYY, MM, DD);
+const convertStringToDate = (date: string) => {
+    const dateSplit = date.split('-');
+    return new Date(parseInt(dateSplit[2]), parseInt(dateSplit[1]), parseInt(dateSplit[0]));
+}
+
+const textDate = (date: string) => {
+    const dateSplit = date.split('-');
+    const year = dateSplit[2];
+    const month = dateSplit[1];
+
+    const monthsList = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    date = monthsList[parseInt(month) - 1] + ' ' + year;
+
+    return date;
+}
+
+
+const sortByDateAsc = (events: any[]) => {
+    return events.sort((a: any, b: any) => {
+        return (+(convertStringToDate(a.full_date)) - +(convertStringToDate(b.full_date)))
+    });
+};
+
+export { convertStringToDate, textDate, sortByDateAsc };
