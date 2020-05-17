1f2dae3021dc258734ef88c71635411596d005fa
Valentin <valentin.nerin@gmail.com>
2020-05-17 08:33:59 +0200

    Change db access with promises

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -9,18 +9,30 @@ const app = express();
 const mongoUrl = 'mongodb://randuser:randpw@mongo:27017/souvenirs-dev';
 
 // Connect to MongoDB
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
-    console.log("connection to mongo opened");
-}).catch(err => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-});
+const connecToMongo = async () => {
+    let db = null;
+    try {
+        await mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+        console.log("connection to mongo opened");
 
+        db = mongoose.connection;
+        db.once('open', () => {
+            console.log("connected to mongo!");
+        })
+    } catch (err) {
+        (db) && db.close();
+        db.on('error', console.error.bind(console, 'connection error:'));
+        throw err;
+    }
+}
+
+connecToMongo();
 // open connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log("connected to mongo!");
-});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => {
+//     console.log("connected to mongo!");
+// });
 
 /**** express configuration ****/
 app.set('port', process.env.port || 8080);
