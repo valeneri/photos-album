f7a04a21271f5a0f5ff72c1b15bb2562b09311e8
Valentin <valentin.nerin@gmail.com>
2020-06-06 21:55:11 +0200

    Styling negative component with embedded images

--- frontend/src/components/negative/negative.tsx
+++ frontend/src/components/negative/negative.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { YearEvents } from "../../shared/models";
-import YearUI from "./year-ui/year-ui";
 import HorizontalScroll from "./horizontal-scroll/horizontal-scroll";
 import "./negative.css";
+import YearUI from "./year-ui/year-ui";
 
 // Negative props interface declaration
 interface NegativeProps {
@@ -27,17 +27,34 @@ const Negative = ({ yearsEventsList, setSelectedYear }: NegativeProps) => {
         setNegativeTranslation(`${scale}`);
     }
 
-    // display years list as events with "real events" next when year is selected
-    const displayYearsEvents = () => yearsEventsList.map((yearEvents: YearEvents) => {
-        return (
-            <div className="years-ui wrapper" key={yearEvents._id}>
-                <div>
-                    <YearUI year={yearEvents} setSelectedYear={setSelectedYear} />
-                    <div style={{ borderRight: `2px solid white` }}></div>
+    // display years list 
+    const displayYearsEvents = () => {
+        return yearsEventsList.map((yearEvents: YearEvents) => {
+            return (
+                <div className="years-ui wrapper" key={yearEvents._id}>
+                    <div>
+                        <YearUI year={yearEvents} setSelectedYear={setSelectedYear} />
+                    </div>
                 </div>
-            </div>
-        )
-    })
+            )
+        })
+    }
+
+    const completeTimeline = () => {
+        const fakeYearsArray = new Array(8 - yearsEventsList.length);
+        const fakeYear = "aaaa"
+        fakeYearsArray.fill(fakeYear)
+        const negativeUrl = `${process.env.PUBLIC_URL}/year-negative-back/test_negatif3.png`;
+        return fakeYearsArray.map((fake, index) => {
+            return (
+                <div className="years-ui wrapper" key={index}>
+                    <div>
+                        <div style={{ backgroundImage: `url('${negativeUrl}')` }} className="year"><h5>{fake.fakeYear}</h5></div>
+                    </div>
+                </div>
+            )
+        })
+    }
 
     return (
         <div>
@@ -46,6 +63,9 @@ const Negative = ({ yearsEventsList, setSelectedYear }: NegativeProps) => {
                 <div className="full-years wrapper" style={setTranslation()}>
                     {
                         displayYearsEvents()
+                    }
+                    {
+                        yearsEventsList.length < 8 && completeTimeline()
                     }
                 </div>
                 <HorizontalScroll yearsEventsList={yearsEventsList} setNegativeTranslation={translateEvents} />
--- frontend/src/components/negative/year-ui/year-ui.tsx
+++ frontend/src/components/negative/year-ui/year-ui.tsx
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./year-ui.css";
 import { YearEvents } from "../../../shared/models";
+import negativeUrl from './negatives-background/test_negatif3.png';
+import negativeUrl1 from './negatives-background/random_year_back.png';
+import negativeUrl2 from './negatives-background/random_year_back2.png';
 
 // EventUi props interface declaration
-interface EventUIProps {
+interface YearUIProps {
     year: YearEvents;
     setSelectedYear: any;
 }
 
-const YearUI = ({ year, setSelectedYear }: EventUIProps) => {
 
-    // set class (event or year) and toggle class if event is selected
-    const setClass = () => {
-        let className = 'year';
+const YearUI = ({ year, setSelectedYear }: YearUIProps) => {
 
-        if (year.selected) {
-            className = className + ' selected';
+    // selected state (for UI)
+    const [selected, setSelected] = useState<string>('');
+    // bakground url
+    const [background, setBackground] = useState<string>('');
+
+    useEffect(() => {
+        const randomBackground = () => {
+            const negativesBackArray = [negativeUrl, negativeUrl1, negativeUrl2];
+            const pos = Math.floor(Math.random() * Math.floor(negativesBackArray.length));
+            setBackground(negativesBackArray[pos]);
         }
-        return className;
+        randomBackground();
+    }, [])
+
+    const handleSelectYear = (year: YearEvents) => {
+        selected === '' ? setSelected('selected') : setSelected('');
+        setSelectedYear(year);
     }
 
     return (
-        <div className={setClass()} onClick={() => setSelectedYear(year)} >
-            <h5>{year.date}</h5>
+        <div style={{ backgroundImage: `url('${background}')` }} className={`year ${selected}`} onClick={() => handleSelectYear(year)} >
+            < h5 > {year.date}</h5 >
             <p>
                 <span>{year.eventsNumber} évènements</span>
             </p>
-        </div>
+        </div >
     )
 }
 
